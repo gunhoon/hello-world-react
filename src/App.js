4266d5e8725c5f4c148cdebb51c34af38649b3c6
Gunhoon Lee <gunhoon@gmail.com>
2020-08-30 00:49:29 +0900

    Remove warning message

--- src/App.js
+++ src/App.js
@@ -4,29 +4,34 @@ function Food({ name, picture }) {
   return (
     <div>
       <h2>I love {name}.</h2>
-      <img src={picture} />
+      <img src={picture} alt={name} />
     </div>
   );
 }
 
 const foodILike = [
   {
+    id: 1,
     name: 'Kimchi',
     image: 'https://i.ytimg.com/vi/mB8c529pd80/maxresdefault.jpg'
   },
   {
+    id: 2,
     name: 'Samgyeopsal',
     image: 'https://i.ytimg.com/vi/IsEVv1Jt2Ro/maxresdefault.jpg'
   },
   {
+    id: 3,
     name: 'Bibimbap',
     image: 'https://i.ytimg.com/vi/6QQ67F8y2b8/maxresdefault.jpg'
   },
   {
+    id: 4,
     name: 'Doncasu',
     image: 'https://i.ytimg.com/vi/gpWSnNYF-rU/maxresdefault.jpg'
   },
   {
+    id: 5,
     name: 'Kimbap',
     image: 'https://i.ytimg.com/vi/2G5SAC3UI3M/maxresdefault.jpg'
   }
@@ -36,7 +41,7 @@ function App() {
   return (
     <div>
       {foodILike.map(dish => (
-        <Food name={dish.name} picture={dish.image} />
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
       ))}
     </div>
   );
