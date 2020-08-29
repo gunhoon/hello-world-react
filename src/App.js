3d6afdbeb294bfa506f1b4a1bcfa057b515f9131
Gunhoon Lee <gunhoon@gmail.com>
2020-08-30 00:01:02 +0900

    Pass array data to Food component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React from 'react';
 
-function Food({ fav }) {
-  return <h3>I love { fav }.</h3>;
+function Food({ name }) {
+  return <h1>I love {name}.</h1>;
 }
+
+const foodILike = [
+  {
+    name: 'Kimchi',
+    image: 'https://i.ytimg.com/vi/mB8c529pd80/maxresdefault.jpg'
+  },
+  {
+    name: 'Samgyeopsal',
+    image: 'https://i.ytimg.com/vi/IsEVv1Jt2Ro/maxresdefault.jpg'
+  },
+  {
+    name: 'Bibimbap',
+    image: 'https://i.ytimg.com/vi/6QQ67F8y2b8/maxresdefault.jpg'
+  },
+  {
+    name: 'Doncasu',
+    image: 'https://i.ytimg.com/vi/gpWSnNYF-rU/maxresdefault.jpg'
+  },
+  {
+    name: 'Kimbap',
+    image: 'https://i.ytimg.com/vi/2G5SAC3UI3M/maxresdefault.jpg'
+  }
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello, World!</h1>
-      <Food fav='kimchi' />
-      <Food fav='ramen' />
-      <Food fav='samgiopsal' />
-      <Food fav='chukumi' />
+      {foodILike.map(dish => (<Food name={dish.name} />))}
     </div>
   );
 }
