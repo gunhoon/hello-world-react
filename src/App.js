067f587cc4c243dc2607b691711f781126b1cc9a
Gunhoon Lee <gunhoon@gmail.com>
2020-08-29 21:55:54 +0900

    Create the Potato component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Potato from './Potato';
 
 function App() {
-  return <div>Hello, World!</div>;
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <Potato />
+    </div>
+  );
 }
 
 export default App;
