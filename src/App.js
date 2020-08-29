2bab0883b35aacdc1897117de7215fceab9f8961
Gunhoon Lee <gunhoon@gmail.com>
2020-08-29 22:34:36 +0900

    Pass props to Food component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
-function Potato() {
-  return <h3>I love potato.</h3>;
+function Food({ fav }) {
+  return <h3>I love { fav }.</h3>;
 }
 
 function App() {
   return (
     <div>
       <h1>Hello, World!</h1>
-      <Potato />
+      <Food fav='kimchi' />
+      <Food fav='ramen' />
+      <Food fav='samgiopsal' />
+      <Food fav='chukumi' />
     </div>
   );
 }
